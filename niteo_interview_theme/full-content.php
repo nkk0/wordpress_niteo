96af9e6da6e474d0291e31330c7a19ef312167f7
nkk0 <klanecek.niko@gmail.com>
2018-06-12 11:59:46 +0200

    Add support for tags

--- niteo_interview_theme/full-content.php
+++ niteo_interview_theme/full-content.php
@@ -1,6 +1,9 @@
 <div class="blog-post">
-  <h2 class="blog-page-title"><?php the_title(); ?></h2>
+  <h2 class="blog-page-title">
+    <?php the_title(); ?>
+  </h2>
 
   <?php the_content(); ?>
+  <?php if (get_the_tags()) {the_tags();} ?>
 
 </div><!-- /.blog-post -->
