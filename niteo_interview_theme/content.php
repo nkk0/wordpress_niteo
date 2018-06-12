061eda4af1d22ca88075a843dc39149b47313728
nkk0 <klanecek.niko@gmail.com>
2018-06-12 09:23:29 +0200

    Add support for sticky posts

--- niteo_interview_theme/content.php
+++ niteo_interview_theme/content.php
@@ -1,5 +1,5 @@
 <div class="blog-post<?php if (($wp_query->current_post + 1) != ($wp_query -> post_count)) {echo ' not-last-post';} ?>">
-  <h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+  <h2 <?php post_class( 'blog-post-title' );?>><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
  <?php echo themeprefix_first_paragraph(); ?>
  <a href="<?php the_permalink(); ?>" class="read-more">READ MORE</a>
