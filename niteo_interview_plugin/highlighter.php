258c9c65653527fa6e5cdbe66683a82cc64b8326
nkk0 <klanecek.niko@gmail.com>
2018-06-13 21:00:20 +0200

    Implement syntax highlightning for posts

--- niteo_interview_plugin/highlighter.php
+++ niteo_interview_plugin/highlighter.php
@@ -6,16 +6,16 @@ Plugin Name: Niko Highlighter
 
 
 function highlight_text($source) {
-    include_once 'geshi.php';
-    $geshi = new GeSHi($source[1], 'python');
+    include_once plugin_dir_path(__FILE__) . 'geshi.php';
+    $geshi = new GeSHi($source[1], 'python3');
 
     return $geshi->parse_code();
 }
 
 function parse_text($input) {
-    $pattern='~```(.*?)```~';
+    $pattern='~```([\s\S]*?)```~';
+    $newstring = preg_replace_callback($pattern,'highlight_text',trim(strip_tags($input)));
 
-    $newstring = preg_replace_callback($pattern,'highlight_text',$input);
     return $newstring;
 }
 
