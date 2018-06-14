def19e84bf02325f7133d9ee60114d723750d3cf
nkk0 <klanecek.niko@gmail.com>
2018-06-14 08:21:27 +0200

    Update plugin info

--- niteo_interview_plugin/highlighter.php
+++ niteo_interview_plugin/highlighter.php
@@ -1,25 +1,34 @@
 <?php
 
 /*
-Plugin Name: Niko Highlighter
+  Plugin Name: Niteo Interview Text Highlighter
+  Version: 1.0
+  Author: nkk0
+  Author URI: github.com/nkk0
 */
 
 
-function highlight_text($source) {
+function highlight_code_syntax($source) {
+  // use the geshi backend to highlight any text for the desired language
   include_once plugin_dir_path(__FILE__) . 'geshi.php';
   $geshi = new GeSHi($source[2], trim($source[1]));
 
   return $geshi->parse_code();
 }
 
-function parse_text($input) {
+function find_code_blocks_in_text($input) {
+  // lazily match any text between 2 sets of ``` and capture specified language
   $pattern='~```(.*)([\s\S]*?)```~';
-  $newstring = preg_replace_callback($pattern,'highlight_text',strip_tags($input));
+
+  // call highlight_text() on every regex match in the $input
+  $newstring = preg_replace_callback($pattern,'highlight_code_syntax',strip_tags($input));
+
+  // replace the invisible 'nbsp' character with an empty string
   $newstring = str_replace("&nbsp;", '', $newstring);
 
   return trim($newstring);
 }
 
-add_filter('the_content', 'parse_text');
+add_filter('the_content', 'find_code_blocks_in_text');
 
 ?>
