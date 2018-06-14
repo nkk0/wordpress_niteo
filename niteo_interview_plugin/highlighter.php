ff4d77a0fd974c5b9fecaf533966271edd341409
nkk0 <klanecek.niko@gmail.com>
2018-06-14 08:22:51 +0200

    Fix comment

--- niteo_interview_plugin/highlighter.php
+++ niteo_interview_plugin/highlighter.php
@@ -7,7 +7,6 @@
   Author URI: github.com/nkk0
 */
 
-
 function highlight_code_syntax($source) {
   // use the geshi backend to highlight any text for the desired language
   include_once plugin_dir_path(__FILE__) . 'geshi.php';
@@ -20,7 +19,7 @@ function find_code_blocks_in_text($input) {
   // lazily match any text between 2 sets of ``` and capture specified language
   $pattern='~```(.*)([\s\S]*?)```~';
 
-  // call highlight_text() on every regex match in the $input
+  // call highlight_code_syntax() on every regex match in the $input
   $newstring = preg_replace_callback($pattern,'highlight_code_syntax',strip_tags($input));
 
   // replace the invisible 'nbsp' character with an empty string
