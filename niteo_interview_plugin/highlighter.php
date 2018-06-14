aa12afaf1b0218d086eac4d6bbb254bfcd7efa74
nkk0 <klanecek.niko@gmail.com>
2018-06-14 08:08:20 +0200

    Add language selection to highlighter

--- niteo_interview_plugin/highlighter.php
+++ niteo_interview_plugin/highlighter.php
@@ -6,17 +6,18 @@ Plugin Name: Niko Highlighter
 
 
 function highlight_text($source) {
-    include_once plugin_dir_path(__FILE__) . 'geshi.php';
-    $geshi = new GeSHi($source[1], 'python3');
+  include_once plugin_dir_path(__FILE__) . 'geshi.php';
+  $geshi = new GeSHi($source[2], trim($source[1]));
 
-    return $geshi->parse_code();
+  return $geshi->parse_code();
 }
 
 function parse_text($input) {
-    $pattern='~```([\s\S]*?)```~';
-    $newstring = preg_replace_callback($pattern,'highlight_text',trim(strip_tags($input)));
+  $pattern='~```(.*)([\s\S]*?)```~';
+  $newstring = preg_replace_callback($pattern,'highlight_text',strip_tags($input));
+  $newstring = str_replace("&nbsp;", '', $newstring);
 
-    return $newstring;
+  return trim($newstring);
 }
 
 add_filter('the_content', 'parse_text');
