ee81b3e42c903ed5be336b8999c79920cea9c111
nkk0 <klanecek.niko@gmail.com>
2018-06-15 13:12:54 +0200

    Fix indentation, add missing line

--- niteo_interview_plugin/niteo_interview_plugin.php
+++ niteo_interview_plugin/niteo_interview_plugin.php
@@ -6,29 +6,33 @@
   Author: nkk0
   Author URI: github.com/nkk0
 */
+
 class SyntaxHighlighter {
-public static function highlight_code_block($source) {
-  // use the geshi backend to highlight any text for the desired language
-  include_once plugin_dir_path(__FILE__) . 'geshi.php';
-  $geshi = new GeSHi(strip_tags($source[2]), trim(strip_tags($source[1])));
+    public static function highlight_code_block($source) {
+        // use the geshi backend to highlight any text for the desired language
+        include_once plugin_dir_path(__FILE__) . 'geshi.php';
+        $geshi = new GeSHi(strip_tags($source[2]), trim(strip_tags($source[1])));
 
-  return $geshi->parse_code();
-}
+        return $geshi->parse_code();
+    }
 
-public static function find_code_blocks_to_highlight($input) {
-	// regex to lazily match any text between 2 sets 
-	// of ``` and capture specified language
-	$pattern='~```(.*)([\s\S]*?)```~';
+    public static function find_code_blocks_to_highlight($input) {
+        // regex to lazily match any text between 2 sets 
+        // of ``` and capture specified language
+        $pattern='~```(.*)([\s\S]*?)```~';
 
-	// call highlight_code_block() on every regex match in the $input
-	$newstring = preg_replace_callback($pattern,'self::highlight_code_block',$input);
+        // call highlight_code_block() on every regex match in the $input
+        $newstring = preg_replace_callback($pattern,'self::highlight_code_block',$input);
 
-	// replace the invisible 'nbsp' character with an empty string
-	$newstring = str_replace("&amp;</span>gt", '></span>', $newstring);
-	$newstring = str_replace("&nbsp;", '', $newstring);
+        // replace the invisible 'nbsp' character with an empty string
+        $newstring = str_replace("&amp;gt", '>', $newstring);
+        $newstring = str_replace("&amp;lt", '<', $newstring);
+        $newstring = str_replace("&amp;</span>gt", '></span>', $newstring);
+        $newstring = str_replace("&amp;</span>lt", '<</span>', $newstring);
+        $newstring = str_replace("&nbsp;", '', $newstring);
 
-	return trim($newstring);
-}
+        return trim($newstring);
+    }
 }
 
 add_filter('the_content', 'SyntaxHighlighter::find_code_blocks_to_highlight');
--- niteo_interview_plugin/tests/test-find-code-blocks.php
+++ niteo_interview_plugin/tests/test-find-code-blocks.php
@@ -7,17 +7,17 @@
 
 class HighlighterIntegrationTest extends WP_UnitTestCase {
 
-	function test_finding_code_blocks() {
-		include('inputs-code-block-finder.php');
+    function test_finding_code_blocks() {
+        include('inputs-code-block-finder.php');
 
-		$codeblocks = SyntaxHighlighter::find_code_blocks_to_highlight($test1[0]);
-		$this->assertEquals($test1[1], $codeblocks);
-		$codeblocks = SyntaxHighlighter::find_code_blocks_to_highlight($test2[0]);
-		$this->assertEquals($test2[1], $codeblocks);
-		$codeblocks = SyntaxHighlighter::find_code_blocks_to_highlight($test3[0]);
-		$this->assertEquals($test3[1], $codeblocks);
+        $codeblocks = SyntaxHighlighter::find_code_blocks_to_highlight($test1[0]);
+        $this->assertEquals($test1[1], $codeblocks);
+        $codeblocks = SyntaxHighlighter::find_code_blocks_to_highlight($test2[0]);
+        $this->assertEquals($test2[1], $codeblocks);
+        $codeblocks = SyntaxHighlighter::find_code_blocks_to_highlight($test3[0]);
+        $this->assertEquals($test3[1], $codeblocks);
 
-	}
+    }
 
 }
 ?>
