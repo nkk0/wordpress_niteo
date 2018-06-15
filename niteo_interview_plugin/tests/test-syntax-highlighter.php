efed3044281931f5894b9025d6d95b2454a6abdd
nkk0 <klanecek.niko@gmail.com>
2018-06-15 08:16:12 +0200

    Update test to use class

--- niteo_interview_plugin/tests/test-syntax-highlighter.php
+++ niteo_interview_plugin/tests/test-syntax-highlighter.php
@@ -7,16 +7,16 @@
 
 class SyntaxHighlighterTest extends WP_UnitTestCase {
 
-	function test_highlighter() {
-		// Test the highlight_code_block function
-		// with some pre-written inputs
-		include('inputs-text-syntax-highlighter.php');
-		$highlighted_text = highlight_code_block($test1[0]);
-		$this->assertEquals($test1[1], $highlighted_text);
-		$highlighted_text = highlight_code_block($test2[0]);
-		$this->assertEquals($test2[1], $highlighted_text);
-		$highlighted_text = highlight_code_block($test3[0]);
-		$this->assertEquals($test3[1], $highlighted_text);
-	}
+    function test_highlighter() {
+        // Test the highlight_code_block function
+        // with some pre-written inputs
+        include('inputs-text-syntax-highlighter.php');
+        $highlighted_text = SyntaxHighlighter::highlight_code_block($test1[0]);
+        $this->assertEquals($test1[1], $highlighted_text);
+        $highlighted_text = SyntaxHighlighter::highlight_code_block($test2[0]);
+        $this->assertEquals($test2[1], $highlighted_text);
+        $highlighted_text = SyntaxHighlighter::highlight_code_block($test3[0]);
+        $this->assertEquals($test3[1], $highlighted_text);
+    }
 }
 ?>
