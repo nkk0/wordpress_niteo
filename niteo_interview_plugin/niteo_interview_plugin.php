06667d694d7ad5816874975def4841b7ae88928c
nkk0 <klanecek.niko@gmail.com>
2018-06-15 07:43:29 +0200

    Rename highlighter and turn it into a class

---
+++ niteo_interview_plugin/niteo_interview_plugin.php
@@ -0,0 +1,36 @@
+<?php
+/*
+  Plugin Name: Niteo Interview Text Highlighter
+  Version: 1.0
+  Author: nkk0
+  Author URI: github.com/nkk0
+*/
+
+class SyntaxHighlighter {
+
+    public static function highlight_code_block($source) {
+        // use the geshi backend to highlight any text for the desired language
+        include_once plugin_dir_path(__FILE__) . 'geshi.php';
+        $geshi = new GeSHi($source[2], trim($source[1]));
+
+        return $geshi->parse_code();
+    }
+
+    public static function find_code_blocks_to_highlight($input) {
+        // regex to lazily match any text between 2 sets 
+        // of ``` and capture specified language
+        $pattern='~```(.*)([\s\S]*?)```~';
+
+        // call highlight_code_block() on every regex match in the $input
+        $newstring = preg_replace_callback($pattern,'self::highlight_code_block',strip_tags($input));
+
+        // replace the invisible 'nbsp' character with an empty string
+        $newstring = str_replace("&nbsp;", '', $newstring);
+
+        return trim($newstring);
+    }
+}
+
+add_filter('the_content', 'SyntaxHighlighter::find_code_blocks_to_highlight');
+
+?>
